8de8373fb76380eb1f4da2dffcab57d5243a9f94
Jeongkyu Shin <inureyes@gmail.com>
2016-06-24 16:37:32 +0900

    refs #2 : add - include.note

---
+++ library/include.note.php
@@ -0,0 +1,39 @@
+<?php
+/// Copyright (c) 2004-2016, Needlworks  / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/documents/LICENSE, /documents/COPYRIGHT)
+
+$context->setProperty('import.library', array(
+    'function.file',
+    'function.string',
+    'function.time',
+    'function.javascript',
+    'function.html',
+    'function.xml',
+    'function.mail',
+    'DEBUG : Basic functions loaded.',
+    'blogskin',
+    'auth',
+    'DEBUG : Default library loaded.',
+    'model.blog.service',
+    'model.blog.blogSetting',
+    'model.blog.category',
+    'model.blog.skin',
+    'model.blog.tag',
+    'model.blog.keyword',
+    'model.blog.archive',
+    'model.blog.page',
+    'model.blog.notice',
+    'model.blog.link',
+    'model.blog.fx',
+    'model.common.plugin',
+    'model.common.module',
+    'model.common.setting',
+    'DEBUG : Models loaded.',
+    'view.html',
+    'view.ownerView',
+    'view.paging',
+    'view.view',
+    'DEBUG : Views loaded.'
+));
+?>
